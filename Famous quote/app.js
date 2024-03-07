6bdb9023eb995ca015498bfd91423fe6615b3e46
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-07 21:52:46 +0500

    Add files via upload

---
+++ Famous quote/app.js
@@ -0,0 +1,3 @@
+var quote = "a person who never made a mistake never tried anything new";
+var author = "Albert einstein";
+console.log("".concat(author, " once said, ").concat(quote));
---
+++ Famous quote/app.ts
@@ -0,0 +1,3 @@
+let quote: string="a person who never made a mistake never tried anything new";
+let author: string="Albert einstein"
+console.log(`${author} once said, ${quote}`);
