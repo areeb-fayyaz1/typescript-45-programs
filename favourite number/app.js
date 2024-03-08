371891a29d5e26f37be0279212be1778b1ff29ec
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-08 23:42:14 +0500

    Add files via upload

---
+++ favourite number/app.js
@@ -0,0 +1,2 @@
+var favouriteNumber = 4;
+console.log("my favourite numer is ".concat(favouriteNumber));
---
+++ favourite number/app.ts
@@ -0,0 +1,2 @@
+let favourite_Number: number =4;
+console.log(`my favourite numer is ${favourite_Number}`);
