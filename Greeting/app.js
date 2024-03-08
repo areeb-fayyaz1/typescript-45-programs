f64091affc03d51e64ade7650044edee85c1a1f9
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-09 00:25:41 +0500

    Add files via upload

---
+++ Greeting/app.js
@@ -0,0 +1,10 @@
+//let members:string[]=['Azam','suqlain','sana','ali','areeb'];
+//for(let i=0; i<members.length; i++){
+//  console.log(members[i]);
+//}
+//exercise 12
+var members = ['Azam', 'suqlain', 'sana', 'ali', 'areeb'];
+var message = "kl birthday hai kisi ki";
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ Greeting/app.ts
@@ -0,0 +1,10 @@
+//let members:string[]=['Azam','suqlain','sana','ali','areeb'];
+//for(let i=0; i<members.length; i++){
+  //  console.log(members[i]);
+//}
+//exercise 12
+let members:string[]=['Azam','suqlain','sana','ali','areeb'];
+let message:string="kl birthday hai kisi ki"
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
