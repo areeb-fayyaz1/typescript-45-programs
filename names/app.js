17894e53e43a4847686a7c93388154c57490e5e8
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-08 23:45:24 +0500

    Add files via upload

---
+++ names/app.js
@@ -0,0 +1,4 @@
+var members = ['Azam', 'suqlain', 'sana', 'ali', 'areeb'];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ names/app.ts
@@ -0,0 +1,4 @@
+let members:string[]=['Azam','suqlain','sana','ali','areeb'];
+for(let i=0; i<members.length; i++){
+    console.log(members[i]);
+}
