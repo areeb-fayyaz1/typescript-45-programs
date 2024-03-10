aae3017f830733aeeed6efdbd62cf689c8762b6e
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-10 23:22:57 +0500

    Add files via upload

---
+++ Guest list/app.js
@@ -0,0 +1,9 @@
+// exercise 13
+// let transportation: string[]=['civic','bus','bike','suzuki','jet'];
+//for(let i=0; i<transportation.length; i++);{
+//  console.log('I would like to own a' + transportation[i]);
+//}
+var guest_list = ['ali', 'farhan', 'raza', 'ayesha'];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected sir/madam' + guest_list[i] + ',\nwe invited you on dinner tomorrow.\n\nThankyou');
+}
---
+++ Guest list/app.ts
@@ -0,0 +1,9 @@
+// exercise 13
+// let transportation: string[]=['civic','bus','bike','suzuki','jet'];
+//for(let i=0; i<transportation.length; i++);{
+  //  console.log('I would like to own a' + transportation[i]);
+//}
+let guest_list:string[]=['ali','farhan','raza','ayesha'];
+for(let i=0; i<guest_list.length; i++){
+    console.log('Respected sir/madam' + guest_list[i] + ',\nwe invited you on dinner tomorrow.\n\nThankyou')
+}
