2c7be910e88f3dba8fd31e9c5c92a351a690eb74
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-11 00:27:11 +0500

    Add files via upload

---
+++ More guest/app.js
@@ -0,0 +1,10 @@
+var guest_list = ['ali', 'farhan', 'raza', 'ayesha'];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected sir/madam' + guest_list[i] + ',\nwe invited you on dinner tomorrow.\n\nThankyou');
+}
+var not_present = "farhan";
+var new_guest = "babar azam";
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected sir/madam' + guest_list[i] + ',\nwe invited you on dinner tomorrow.\n\nThankyou\n');
+}
+guest_list.unshift('shahin', 'sarfraz', 'rizwam');
---
+++ More guest/app.ts
@@ -0,0 +1,15 @@
+let guest_list:string[]=['ali','farhan','raza','ayesha'];
+for(let i=0; i<guest_list.length; i++){
+    console.log('Respected sir/madam' + guest_list[i] + ',\nwe invited you on dinner tomorrow.\n\nThankyou')
+}
+let not_present:string="farhan";
+let new_guest:string="babar azam";
+for(let i=0; i<guest_list.length; i++){
+console.log('Respected sir/madam' + guest_list[i] + ',\nwe invited you on dinner tomorrow.\n\nThankyou\n')
+
+}
+guest_list.unshift('shahin','sarfraz','rizwam');
+for(let i=0; i<guest_list.length; i++){
+    console.log('Respected sir/madam' + guest_list[i] + ',\nwe invited you on dinner tomorrow because we have extra table .\n\nThankyou\n')
+    }
+
