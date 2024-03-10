7b2f15107ce9be609b30b1bb14caf6747e7ce47c
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-10 23:21:39 +0500

    Add files via upload

---
+++ Your own array/app.js
@@ -0,0 +1,7 @@
+// exercise 13
+var transportation = ['civic', 'bus', 'bike', 'suzuki', 'jet'];
+for (var i = 0; i < transportation.length; i++)
+    ;
+{
+    console.log('I would like to own a' + transportation);
+}
---
+++ Your own array/app.ts
@@ -0,0 +1,5 @@
+// exercise 13
+let transportation: string[]=['civic','bus','bike','suzuki','jet'];
+for(let i=0; i<transportation.length; i++);{
+    console.log('I would like to own a' + transportation[i]);
+}
