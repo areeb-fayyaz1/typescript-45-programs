0a2bcc4d4223cf0d74936abc2a50f474111ed568
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-11 00:29:31 +0500

    Add files via upload

---
+++ Seeing the world/app.ts
@@ -0,0 +1,7 @@
+let places :string =['karachi','lahore','multan','islamabad','peshawar']
+console.log('original:' + places)
+console.log('copy:' + [...places].sort().reverse());
+console.log('copy:' + [...places].sort().reverse());
+
+
+
