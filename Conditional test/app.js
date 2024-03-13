18aed0d55867cc510575f0cc8c0b7bfd64b3ce61
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-13 23:16:03 +0500

    Add files via upload

---
+++ Conditional test/app.js
@@ -0,0 +1,25 @@
+var cars = 'sabaru';
+//test 1;equality comparision (true)
+console.log("is car =='sabaru'? i predict true.");
+console.log(cars == 'sabaru'); //true
+// test 2 strict quality comparision(true)
+console.log("is car ==='sabaru'? i predict true.");
+console.log(cars === 'sabaru'); //true
+//test 3 inequality comparision(false)
+console.log("is car !='sabaru'? i predict false.");
+console.log(cars != 'sabaru'); //false
+//test 4 strict inequality comparision(false)
+console.log("is car !=='sabaru'? i predict false.");
+console.log(cars !== 'sabaru'); //false
+// test 5 less than comparision(false)
+console.log("is car <'sabaru'? i predict true.");
+console.log(cars < 'sabaru'); //false
+// test 6 greater than comparision(false)
+console.log("is car >'sabaru'? i predict true.");
+console.log(cars > 'sabaru'); //true
+//test 7 less than or equal comparision(true)
+console.log("is car =='sabaru'? i predict true.");
+console.log(cars == 'sabaru'); //true
+//test 8
+console.log("is car =='sabaru'? i predict true.");
+console.log(cars == 'sabaru'); //true
---
+++ Conditional test/app.ts
@@ -0,0 +1,35 @@
+let cars: string='sabaru';
+
+//test 1;equality comparision (true)
+console.log("is car =='sabaru'? i predict true.");
+console.log(cars=='sabaru');//true
+
+// test 2 strict quality comparision(true)
+console.log("is car ==='sabaru'? i predict true.");
+console.log(cars==='sabaru');//true
+
+//test 3 inequality comparision(false)
+console.log("is car !='sabaru'? i predict false.");
+console.log(cars!='sabaru');//false
+
+//test 4 strict inequality comparision(false)
+console.log("is car !=='sabaru'? i predict false.");
+console.log(cars!=='sabaru');//false
+
+// test 5 less than comparision(false)
+console.log("is car <'sabaru'? i predict true.");
+console.log(cars<'sabaru');//false
+
+// test 6 greater than comparision(false)
+console.log("is car >'sabaru'? i predict true.");
+console.log(cars>'sabaru');//true
+
+//test 7 less than or equal comparision(true)
+console.log("is car <='sabaru'? i predict true.");
+console.log(cars<='sabaru');//true
+
+//test 8 greater than or equal comparision
+console.log("is car >='sabaru'? i predict true.");
+console.log(cars>='sabaru');//true
+
+
