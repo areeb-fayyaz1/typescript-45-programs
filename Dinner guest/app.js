47a36268c8b780db74358fa4d8d94a66bcaf617e
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-13 23:04:43 +0500

    Add files via upload

---
+++ Dinner guest/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var app_js_1 = require("../program 14/app.js");
+console.log(templateObject_1 || (templateObject_1 = __makeTemplateObject(["n PRINTING NUMBER OF GUEST INVITED"], ["n\\ PRINTING NUMBER OF GUEST INVITED"])));
+console.log(templateObject_2 || (templateObject_2 = __makeTemplateObject(["we finally invited ", " from exercise 14"], ["we finally invited ", " from exercise 14"])), app_js_1.guest_list.length);
+var templateObject_1, templateObject_2;
---
+++ Dinner guest/app.ts
@@ -0,0 +1,3 @@
+import{ guest_list } from '../program 14/app.js'
+console.log`n\ PRINTING NUMBER OF GUEST INVITED`
+console.log`we finally invited ${guest_list.length} from exercise 14`
