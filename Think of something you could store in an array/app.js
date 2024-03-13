1d8f96f0d8b1bd97df7e5a7912e504872879f071
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-13 23:06:25 +0500

    Add files via upload

---
+++ Think of something you could store in an array/app.js
@@ -0,0 +1,6 @@
+var languages = ['hindi', 'english', 'urdu', 'french', 'hindi'];
+console.log("list of languages:");
+for (var _i = 0, languages_1 = languages; _i < languages_1.length; _i++) {
+    var top_1 = languages_1[_i];
+    console.log(top_1);
+}
---
+++ Think of something you could store in an array/app.ts
@@ -0,0 +1,5 @@
+let languages: string[]=['hindi','english','urdu','french','hindi']
+console.log("list of languages:")
+for (let top of languages){
+    console.log(top)
+}
