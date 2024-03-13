fa847670e32af384c6926902ccfe03d78e328197
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-13 23:08:43 +0500

    Add files via upload

---
+++ Think of something you could store in typescript object/app.js
@@ -0,0 +1,11 @@
+// create two objects
+var book = {
+    name: 'Essential typescrist',
+    price: 450
+};
+var apple = {
+    name: 'apple',
+    price: 200
+};
+console.log("book name: ".concat(book.net, ", price: $").concat(book.price));
+console.log("apple name: ".concat(apple.net, ", price: $").concat(apple.price));
---
+++ Think of something you could store in typescript object/app.ts
@@ -0,0 +1,15 @@
+interface item{
+    name:string
+    price:number
+}
+// create two objects
+const book: item ={
+    name: 'Essential typescrist',
+    price:450
+}
+const apple: item ={
+    name:'apple'
+    price:200
+}
+console.log(`book name: ${book.net}, price: $${book.price}`)
+console.log(`apple name: ${apple.net}, price: $${apple.price}`)
