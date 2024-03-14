65ee91b280d09724287a167de9c1297c6ab56bb6
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-14 17:42:11 +0500

    Add files via upload

---
+++ Favourite fruit/app.js
@@ -0,0 +1,17 @@
+// Question # 29: Favorite Fruit
+var favorite_fruits = ["banana", "apple", "mango"];
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mangoes!");
+}
+if (favorite_fruits.includes("cherry")) {
+    console.log("You really like cherries!");
+}
+else {
+    console.log("You don't like cherries>");
+}
---
+++ Favourite fruit/app.ts
@@ -0,0 +1,22 @@
+// Question # 29: Favorite Fruit
+
+let favorite_fruits: string[] = ["banana", "apple", "mango"];
+
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like bananas!");
+}
+
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apples!");
+}
+
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mangoes!");   
+}
+
+if (favorite_fruits.includes("cherry")) {
+    console.log("You really like cherries!")
+} else {
+    console.log("You don't like cherries>");
+    
+}
