1b5e72902ef03683663b523b62e9ed2b4fb07c13
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-14 17:40:27 +0500

    Add files via upload

---
+++ Stages of life/app.ts
@@ -0,0 +1,14 @@
+let age = 18
+if (age < 2) {
+    console.log("You are a baby.")
+} else if (age < 4) {
+    console.log("You are a toddler.")
+} else if (age < 13) {
+    console.log("You are a kid.");
+} else if (age < 20) {
+    console.log("You are a teenager.");
+} else if (age < 65) {
+    console.log("You are an adult. ");
+} else {
+    console.log("You are an elder ");
+}
