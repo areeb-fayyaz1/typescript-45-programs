6f5bcb1d7cdd58b746c9d11aa347d4553cc5671c
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-14 17:36:49 +0500

    Add files via upload

---
+++ Alien colours no1,2,3/app.js
@@ -0,0 +1,51 @@
+// Question # 25: Alien Colors #1: 
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points.");
+}
+alien_color = "red";
+if (alien_color == "green") {
+    //no output because condition is false
+}
+//Question # 26: Alien Colors #2:
+alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("You just earned 10 points.");
+}
+//Question # 27: Alien Colors #3: 
+//If the alien is green
+alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points!");
+}
+else if (alien_color == "yellow") {
+    console.log("You earned 10 points.");
+}
+else if (alien_color == "red") {
+    console.log("You earned 15 points.");
+}
+// If the alien is yellow
+alien_color = "yellow";
+if (alien_color == "green") {
+    console.log("You just earned 5 points!");
+}
+else if (alien_color == "yellow") {
+    console.log("You earned 10 points.");
+}
+else if (alien_color == "red") {
+    console.log("You earned 15 points.");
+}
+// If the alien is red
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("You just earned 5 points!");
+}
+else if (alien_color == "yellow") {
+    console.log("You earned 10 points.");
+}
+else if (alien_color == "red") {
+    console.log("You earned 15 points.");
+}
