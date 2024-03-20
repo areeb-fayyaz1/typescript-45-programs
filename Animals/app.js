24d9e941f31e3f1f16acce6ff7e4e7e034181e31
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-20 17:45:51 +0500

    Add files via upload

---
+++ Animals/app.js
@@ -0,0 +1,6 @@
+// Question # 35: Animals
+var animals = ["dog", "cat", "rabbit"];
+animals.forEach(function (animal) {
+    console.log("A ".concat(animal, " would make a great pet."));
+});
+console.log("Any of these animals would make a great pet!");
