dc15bed6cf2a301a62da70a5b5b9162284719177
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-03-20 17:44:54 +0500

    Add files via upload

---
+++ Pizzas/app.js
@@ -0,0 +1,6 @@
+// Question # 34: Pizzas
+var pizzas = ["pepporoni", "margherita", "hawaiian"];
+pizzas.forEach(function (pizza) {
+    console.log("I like ".concat(pizza, " pizza."));
+});
+console.log("I really love pizza!");
