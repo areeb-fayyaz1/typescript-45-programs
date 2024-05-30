d73049f5b67e3ef7488e790c83850f8cfddb9422
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-05-30 15:37:39 +0500

    Add files via upload

---
+++ Great magicians/app.js
@@ -0,0 +1,14 @@
+//Question # 42. Great Magicians
+var magicians = ["Alice", "David", "Chris"];
+function show_magicians(magicians) {
+    magicians.forEach(function (magician) {
+        console.log(magician);
+    });
+}
+function make_great(magicians) {
+    for (var i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] + " the Great";
+    }
+}
+make_great(magicians);
+show_magicians(magicians);
