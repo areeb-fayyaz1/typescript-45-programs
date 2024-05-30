5a3948dfe1dd936a1a99c7ea1be9f4400422dabd
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-05-30 15:52:41 +0500

    Add files via upload

---
+++ Sandwiches/app.ts
@@ -0,0 +1,9 @@
+//Question # 44. Sandwiches 
+
+function make_sandwich(...items: string[]) {
+    console.log(`Making a sandwich with: ${items.join(',')}.`);
+}
+
+make_sandwich("ham","cheese")
+make_sandwich("turkey","lettuce","tomato")
+make_sandwich("avocado","sprouts","mustrd","mayo")
