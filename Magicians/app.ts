01a3f38fd2ef7520839593cd597af353d1c83a7d
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-05-30 15:36:18 +0500

    Add files via upload

---
+++ Magicians/app.ts
@@ -0,0 +1,12 @@
+//Question #41. Magicians
+
+let magicians: string[] = ["Alice", "David", "Charlie"]
+
+function show_magicians(magicians:string[]) {
+    magicians.forEach(magician => 
+        {
+            console.log(magician);
+        })
+}
+
+show_magicians(magicians)
