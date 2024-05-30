ccc661b87ad6ca0cac6324fe414a41e499cca197
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-05-30 15:31:06 +0500

    Add files via upload

---
+++ City names/app.js
@@ -0,0 +1,7 @@
+//Question # 39: City Names
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "France"));
