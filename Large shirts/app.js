aadb2eaf80834a86a3aaa9349a4cb245f4ac546a
Areeb fayyaz <162181111+areeb-fayyaz1@users.noreply.github.com>
2024-05-30 15:28:09 +0500

    Add files via upload

---
+++ Large shirts/app.js
@@ -0,0 +1,9 @@
+// Question # 37: Large Shirts
+function make_shirt(size, message) {
+    if (size === void 0) { size = "large"; }
+    if (message === void 0) { message = "I love TypeScript"; }
+    console.log("Making a ".concat(size, " t-shirt with the message \"").concat(message, "\" printed on it."));
+}
+make_shirt();
+make_shirt("medium");
+make_shirt("small", "Dive into coding");
---
+++ Large shirts/app.ts
@@ -0,0 +1,10 @@
+// Question # 37: Large Shirts
+
+function make_shirt(size: string = "large", message: string = "I love TypeScript")
+{
+    console.log(`Making a ${size} t-shirt with the message "${message}" printed on it.`);
+}
+
+make_shirt()
+make_shirt("medium")
+make_shirt("small", "Dive into coding")
